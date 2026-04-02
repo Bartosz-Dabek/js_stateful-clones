4f4ed99691cd4cc0a01755775bd1dc1259f42821
Bartosz <bartek1098@gmail.com>
2026-04-02 13:08:36 +0200

    Solved excercise

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -7,7 +7,34 @@
  * @return {Object[]}
  */
 function transformStateWithClones(state, actions) {
-  // write code here
+  const result = [];
+
+  let currentState = { ...state }; // kopiujemy początek
+
+  for (const action of actions) {
+
+    // robimy NOWĄ kopię przed zmianą
+    let newState = { ...currentState };
+
+    if (action.type === 'addProperties') {
+      Object.assign(newState, action.extraData);
+    }
+
+    if (action.type === 'removeProperties') {
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+    }
+
+    if (action.type === 'clear') {
+      newState = {};
+    }
+
+    result.push(newState);
+    currentState = newState;
+  }
+
+  return result;
 }
 
 module.exports = transformStateWithClones;
